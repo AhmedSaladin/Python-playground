44893335754c86f607bc93ac02e85a63ae05608f
Ahmed Saladin <as.yuri74@gmail.com>
2020-12-09 14:17:22 +0200

    update templates

--- django/blog/views.py
+++ django/blog/views.py
@@ -1,11 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here (The logic).
 
 def home (req):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(req,'blog/home.html')
 
 def about(req):
-        return HttpResponse('<h1>Blog About</h1>')
+        return render(req,'blog/about.html')
 
