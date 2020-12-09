ea37e2f7f09d92d95424bbded4b8468d34770b61
Ahmed Saladin <as.yuri74@gmail.com>
2020-12-09 14:22:38 +0200

    Dummy data

--- django/blog/views.py
+++ django/blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-
 # Create your views here (The logic).
 
-def home (req):
-    return render(req,'blog/home.html')
+
+# Create dummy data to test
+posts = [{
+    'author': 'coryMs',
+    'title': 'post 1',
+    'content': 'First post content',
+    'date_posted': 'august 27,2018'
+},
+    {
+    'author': 'Jane Doe',
+    'title': 'post 12',
+    'content': 'Second post content',
+    'date_posted': 'august 28,2018'
+}]
+
+
+def home(req):
+    return render(req, 'blog/home.html')
+
 
 def about(req):
-        return render(req,'blog/about.html')
-
+    return render(req, 'blog/about.html')
