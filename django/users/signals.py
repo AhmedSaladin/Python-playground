fee28e96e3de0adf689e45e57f3caa3b1692d61e
Ahmed Saladin <as.yuri74@gmail.com>
2020-12-23 14:15:26 +0200

    adding signals to link profile with created user

---
+++ django/users/signals.py
@@ -0,0 +1,17 @@
+# This make when new user sign up in application
+# Create profile to it (link application events together)
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
