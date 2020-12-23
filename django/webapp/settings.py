dd4ddaf8911b093210142445a7f7ae8bf2e08a78
Ahmed Saladin <as.yuri74@gmail.com>
2020-12-23 11:53:25 +0200

    ading login and logout pages

--- django/webapp/settings.py
+++ django/webapp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from django.contrib.auth import login
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,5 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+# Change crispy package default styles
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+# Change login default redirect page
+LOGIN_REDIRECT_URL = 'blog-home'
