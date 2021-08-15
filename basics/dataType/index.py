7ce1efa8af467fe449f7e7312a00d44ae0c9ce6d
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-15 17:18:41 +0200

    Solving Quiz: Total Sales

--- basics/dataType/index.py
+++ basics/dataType/index.py
@@ -69,3 +69,15 @@ name_length = name_length = len(given_name + " " + middle_names + " " + family_n
 # Nothing you need to do here
 driving_license_character_limit = 28
 print(name_length <= driving_license_character_limit)
+
+
+mon_sales = "121"
+tues_sales = "105"
+wed_sales = "110"
+thurs_sales = "98"
+fri_sales = "95"
+
+#TODO: Print a string with this format: This week's total sales: xxx
+# You will probably need to write some lines of code before the print statement.
+week_total_sales= int(mon_sales)+int(tues_sales)+int(wed_sales)+int(thurs_sales)+int(fri_sales)
+print("This week's total sales: "+str(week_total_sales))
