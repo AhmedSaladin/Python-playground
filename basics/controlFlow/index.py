9292aedc86aae9ccc4f3530a5b4dac4c9cd97ca4
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-17 21:24:08 +0200

    Solving Quiz: Using Truth Values of Objects

--- basics/controlFlow/index.py
+++ basics/controlFlow/index.py
@@ -13,8 +13,8 @@ print(result)
 
 
 # '''
-# You decide you want to play a game where you are hiding 
-# a number from someone.  Store this number in a variable 
+# You decide you want to play a game where you are hiding
+# a number from someone.  Store this number in a variable
 # called 'answer'.  Another user provides a number called
 # 'guess'.  By comparing guess to answer, you inform the user
 # if their guess is too high or too low.
@@ -36,29 +36,53 @@ print(result)
 
 
 # '''
-# Depending on where an individual is from we need to tax them 
-# appropriately.  The states of CA, MN, and 
+# Depending on where an individual is from we need to tax them
+# appropriately.  The states of CA, MN, and
 # NY have taxes of 7.5%, 9.5%, and 8.9% respectively.
-# Use this information to take the amount of a purchase and 
+# Use this information to take the amount of a purchase and
 # the corresponding state to assure that they are taxed by the right
 # amount.
 # '''
 state = "CA"
 purchase_amount = 150
 
-if  state== 'CA':
-    tax_amount = .075
-    total_cost = purchase_amount*(1+tax_amount)
+if state == "CA":
+    tax_amount = 0.075
+    total_cost = purchase_amount * (1 + tax_amount)
     result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
 
-elif  state == 'MN':
-    tax_amount = .095
-    total_cost = purchase_amount*(1+tax_amount)
+elif state == "MN":
+    tax_amount = 0.095
+    total_cost = purchase_amount * (1 + tax_amount)
     result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
 
-elif state == 'NY':
-    tax_amount = .089
-    total_cost = purchase_amount*(1+tax_amount)
+elif state == "NY":
+    tax_amount = 0.089
+    total_cost = purchase_amount * (1 + tax_amount)
     result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+print(result)
+
+
+points = 174  # use this as input for your submission
+
+# establish the default prize value to None
+prize = None
+
+# use the points value to assign prizes to the correct prize names
+if points <= 50:
+    prize = "wooden rabbit!"
+elif points <= 150:
+    prize = None
+elif points <= 180:
+    prize = "wafer-thin mint"
+else:
+    prize = "penguin"
+
+# use the truth value of prize to assign result to the correct prize
+if prize:
+    result = "Congratulations! You won a {}!".format(prize)
+else:
+    result = "Oh dear, no prize this time."
 
 print(result)
