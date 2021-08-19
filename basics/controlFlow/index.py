bb177641dfd7e5e681f22652b05700bedee03499
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-19 03:27:19 +0200

    Solving Quiz: Nearest Square

--- basics/controlFlow/index.py
+++ basics/controlFlow/index.py
@@ -145,63 +145,72 @@ for key, value in basket_items.items():
 print(result)
 
 
-#Example 1
+# Example 1
 
 result = 0
-basket_items = {'pears': 5, 'grapes': 19, 'kites': 3, 'sandwiches': 8, 'bananas': 4}
-fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+basket_items = {"pears": 5, "grapes": 19, "kites": 3, "sandwiches": 8, "bananas": 4}
+fruits = ["apples", "oranges", "pears", "peaches", "grapes", "bananas"]
 
-#Iterate through the dictionary
-for key,value in basket_items.items():
-#if the key is in the list of fruits, add the value (number of fruits) to result
-    if(key in fruits):
-        result+= value
+# Iterate through the dictionary
+for key, value in basket_items.items():
+    # if the key is in the list of fruits, add the value (number of fruits) to result
+    if key in fruits:
+        result += value
 
 print(result)
 
-#Example 2
+# Example 2
 
 result = 0
-basket_items = {'peaches': 5, 'lettuce': 2, 'kites': 3, 'sandwiches': 8, 'pears': 4}
-fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+basket_items = {"peaches": 5, "lettuce": 2, "kites": 3, "sandwiches": 8, "pears": 4}
+fruits = ["apples", "oranges", "pears", "peaches", "grapes", "bananas"]
 
-#Iterate through the dictionary
-for key,value in basket_items.items():
-#if the key is in the list of fruits, add the value (number of fruits) to result
-    if(key in fruits):
-        result+= value
+# Iterate through the dictionary
+for key, value in basket_items.items():
+    # if the key is in the list of fruits, add the value (number of fruits) to result
+    if key in fruits:
+        result += value
 print(result)
 
 
-#Example 3
+# Example 3
 
 result = 0
-basket_items = {'lettuce': 2, 'kites': 3, 'sandwiches': 8, 'pears': 4, 'bears': 10}
-fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+basket_items = {"lettuce": 2, "kites": 3, "sandwiches": 8, "pears": 4, "bears": 10}
+fruits = ["apples", "oranges", "pears", "peaches", "grapes", "bananas"]
 
-#Iterate through the dictionary
-for key,value in basket_items.items():
-#if the key is in the list of fruits, add the value (number of fruits) to result
-    if(key in fruits):
-        result+= value
+# Iterate through the dictionary
+for key, value in basket_items.items():
+    # if the key is in the list of fruits, add the value (number of fruits) to result
+    if key in fruits:
+        result += value
 print(result)
 
 
-# You would like to count the number of fruits in your basket. 
+# You would like to count the number of fruits in your basket.
 # In order to do this, you have the following dictionary and list of
 # fruits.  Use the dictionary and list to count the total number
 # of fruits and not_fruits.
 
 fruit_count, not_fruit_count = 0, 0
-basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
-fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+basket_items = {"apples": 4, "oranges": 19, "kites": 3, "sandwiches": 8}
+fruits = ["apples", "oranges", "pears", "peaches", "grapes", "bananas"]
 
-for key,value in basket_items.items():
-#if the key is in the list of fruits, add the value (number of fruits) to result
-    if(key in fruits):
-        fruit_count+= value
-#if the key is not in the list, then add to the not_fruit_count
+for key, value in basket_items.items():
+    # if the key is in the list of fruits, add the value (number of fruits) to result
+    if key in fruits:
+        fruit_count += value
+    # if the key is not in the list, then add to the not_fruit_count
     else:
-        not_fruit_count+=value
+        not_fruit_count += value
 
 print(fruit_count, not_fruit_count)
+
+limit = 40
+
+num = 0
+while (num + 1) ** 2 < limit:
+    num += 1
+nearest_square = num ** 2
+
+print(nearest_square)
