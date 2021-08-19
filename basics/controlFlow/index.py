2f0f6cc4f664751bd3261e36e39b8b9c838a27e1
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-19 03:52:37 +0200

    Solving Coding Quiz: Check for Prime Numbers

--- basics/controlFlow/index.py
+++ basics/controlFlow/index.py
@@ -233,3 +233,19 @@ for headline in headlines:
         break
 
 print(news_ticker)
+
+
+## Your code should check if each number in the list is a prime number
+check_prime = [26, 39, 51, 53, 57, 79, 85]
+
+## write your code here
+## HINT: You can use the modulo operator to find a factor
+for number in check_prime:
+    if number % 2 == 0:
+        print(
+            "{} is NOT a prime number, because {} is a factor of {}".format(
+                number, 2, number
+            )
+        )
+    else:
+        print("{} IS a prime number".format(number))
