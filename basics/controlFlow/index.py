d329dc065af5f78c5480241adf4b7bccebd63c98
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-19 03:47:11 +0200

    Solving Quiz: Break the String

--- basics/controlFlow/index.py
+++ basics/controlFlow/index.py
@@ -214,3 +214,22 @@ while (num + 1) ** 2 < limit:
 nearest_square = num ** 2
 
 print(nearest_square)
+
+
+headlines = [
+    "Local Bear Eaten by Man",
+    "Legislature Announces New Laws",
+    "Peasant Discovers Violence Inherent in System",
+    "Cat Rescues Fireman Stuck in Tree",
+    "Brave Knight Runs Away",
+    "Papperbok Review: Totally Triffic",
+]
+
+news_ticker = ""
+for headline in headlines:
+    news_ticker += headline + " "
+    if len(news_ticker) >= 140:
+        news_ticker = news_ticker[:140]
+        break
+
+print(news_ticker)
