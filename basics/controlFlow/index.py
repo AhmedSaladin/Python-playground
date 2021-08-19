e3cd95a8ab4eb03f89066a3113885465eff67a3d
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-19 04:42:45 +0200

    Solving Quiz: Filter Names by Scores

--- basics/controlFlow/index.py
+++ basics/controlFlow/index.py
@@ -324,3 +324,25 @@ names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smi
 
 first_names = [name.lower() for name in names]
 print(first_names)
+
+
+# Quiz: Multiples of Three
+# Use a list comprehension to create a list multiples_3 containing the first 20 multiples of 3.
+multiples_3 = [i * 3 for i in range(1, 21)]
+print(len(multiples_3))
+print(multiples_3)
+
+
+
+# Quiz: Filter Names by Scores
+# Use a list comprehension to create a list of names passed that only include those that scored at least 65.
+scores = {
+    "Rick Sanchez": 70,
+    "Morty Smith": 35,
+    "Summer Smith": 82,
+    "Jerry Smith": 23,
+    "Beth Smith": 98,
+}
+
+passed = [name for name, value in scores.items() if value > 65]
+print(passed)
