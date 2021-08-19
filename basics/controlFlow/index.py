0260ad804bf31ac622ed6d192f2f6cc97230bbba
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-19 04:28:30 +0200

    Solving Quiz: Enumerate

--- basics/controlFlow/index.py
+++ basics/controlFlow/index.py
@@ -295,3 +295,17 @@ names,heights =zip(*cast)
 
 print(names)
 print(heights)
+
+
+# Quiz: Enumerate
+# Use enumerate to modify the cast list so that each element contains the name followed by the character's corresponding height.
+#  For example, the first element of cast should change from "Barney Stinson" to "Barney Stinson 72".
+
+cast = ["Barney Stinson", "Robin Scherbatsky", "Ted Mosby", "Lily Aldrin", "Marshall Eriksen"]
+heights = [72, 68, 72, 66, 76]
+
+# write your for loop here
+for i,height in enumerate(heights):
+    cast[i]+=" "+str(height)
+
+print(cast)
