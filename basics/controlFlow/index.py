708818becb12884f454c2d994a65682f5216c639
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-19 02:58:58 +0200

    Solving Quiz: Fruit Basket - Task 2

--- basics/controlFlow/index.py
+++ basics/controlFlow/index.py
@@ -143,3 +143,45 @@ for key, value in basket_items.items():
     if key in fruits:
         result += value
 print(result)
+
+
+#Example 1
+
+result = 0
+basket_items = {'pears': 5, 'grapes': 19, 'kites': 3, 'sandwiches': 8, 'bananas': 4}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for key,value in basket_items.items():
+#if the key is in the list of fruits, add the value (number of fruits) to result
+    if(key in fruits):
+        result+= value
+
+print(result)
+
+#Example 2
+
+result = 0
+basket_items = {'peaches': 5, 'lettuce': 2, 'kites': 3, 'sandwiches': 8, 'pears': 4}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for key,value in basket_items.items():
+#if the key is in the list of fruits, add the value (number of fruits) to result
+    if(key in fruits):
+        result+= value
+print(result)
+
+
+#Example 3
+
+result = 0
+basket_items = {'lettuce': 2, 'kites': 3, 'sandwiches': 8, 'pears': 4, 'bears': 10}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for key,value in basket_items.items():
+#if the key is in the list of fruits, add the value (number of fruits) to result
+    if(key in fruits):
+        result+= value
+print(result)
