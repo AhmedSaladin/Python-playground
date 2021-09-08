17b50c28dec893577be8bea642b00e04b30b4c33
Ahmed Saladin <as.yuri74@gmail.com>
2021-09-08 17:55:47 +0200

    Writing test case

--- CS212/PokerGame/main.py
+++ CS212/PokerGame/main.py
@@ -5,3 +5,15 @@ def poker(hands):
 
 def hand_rank(hand):
     return
+
+
+def test():
+    "Test case for poker function"
+    sf = "6C 7C 8C 9C TC".split()
+    fk = "9D 9H 9S 9C 7D".split()
+    fh = "TD TC TH 7C 7D".split()
+    assert poker([sf, fk, fh]) == sf
+    return "tests pass"
+
+
+print(test())
