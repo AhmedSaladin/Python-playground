105f6111772d76571905f5e7ae23886493e80e8c
Ahmed Saladin <as.yuri74@gmail.com>
2021-09-08 18:06:57 +0200

    Adding more test cases

--- CS212/PokerGame/main.py
+++ CS212/PokerGame/main.py
@@ -13,6 +13,10 @@ def test():
     fk = "9D 9H 9S 9C 7D".split()
     fh = "TD TC TH 7C 7D".split()
     assert poker([sf, fk, fh]) == sf
+    assert poker([fk, fh]) == fk
+    assert poker([fh, fh]) == fh
+    assert poker([sf]) == sf
+    assert poker([sf] + 99 * [fh]) == sf
     return "tests pass"
 
 
